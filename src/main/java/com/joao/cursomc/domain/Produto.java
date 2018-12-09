f43cfcd0388e7e0d66acbbc5773f0f6b2b2ed705
Joao Henrique <joao.progat@gmail.com>
2018-12-09 09:48:26 -0200

    Exception

--- src/main/java/com/joao/cursomc/domain/Produto.java
+++ src/main/java/com/joao/cursomc/domain/Produto.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Produto implements Serializable {
 
@@ -24,13 +26,17 @@ public class Produto implements Serializable {
 	private Integer id;
 	private String nome;
 	private double preco;
-
+	@JsonBackReference
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA", 
 			   joinColumns = @JoinColumn(name = "produto_id"), 
 			   inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
-
+	
+	public Produto() {
+		
+	}
+	
 	public Produto(Integer id, String nome, double preco) {
 		super();
 		this.id = id;
