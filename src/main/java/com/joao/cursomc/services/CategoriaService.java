3c4d0917ca01dc9a12d5e6ad04d7da8c7591c635
Joao Henrique <joao.progat@gmail.com>
2019-02-19 20:36:32 -0300

    Dto categorias

--- src/main/java/com/joao/cursomc/services/CategoriaService.java
+++ src/main/java/com/joao/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.joao.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,5 +40,9 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não e possivel excluir uma categoria que possui produtos");
 		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repository.findAll();
 	}
 }
